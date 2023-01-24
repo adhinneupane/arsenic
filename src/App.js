acd80f1b54f494f50422bba7258704fb576c8a81
adhinneupane <adhinneupane2020@gmail.com>
2023-01-24 06:37:15 -0600

    bootstrap

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bootstrap react applications with vercel and circleCI 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React app with vercel and circleCI
+          Learn React
         </a>
       </header>
     </div>
